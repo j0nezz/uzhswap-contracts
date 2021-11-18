13e76d70d8e9ef4602929b38ff5c7d35871bd313
Tobias Boner <tobiasboner@hispeed.ch>
2021-11-18 15:53:37 +0100

    implement changes and pr comments

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ In the root folder create a `.env` file with the following values:
     GANACHE_URL="http://127.0.0.1:7545"
     UZH_URL="http://130.60.244.246:8545"  
 
-## Deployment
+## Deployment  Uniswap Contracts
 
 ```  
 hardhat run scripts/deploy.ts --network uzh  
@@ -31,10 +31,23 @@ Following contracts have been deployed on the UZHETH and are ready to use.
 | Multicall   | 0x9F2a04D94C6EA38965A854c3859C88c1E2Eb0BA0 |   
 | Quoter   | 0xffF309A1CA21cdC63d6E1782D8D362aA26BDe480 |
 
-### Deploy Tokens
+## Deployment Tokens
 
 ```  
 hardhat run scripts/deploy-erc20-tokens.ts --network uzh  
 ```  
 
 The addresses of your deployed token will be stored locally in the `tokenAddresses.json` file for you to check later.
+
+### Token Addresses
+
+| Contract        | Address     |  
+| ------------- |:-------------:| 
+| UzhUniToken (UZHUNI):  | 0xE771E7A06abDC5176C9D20365c844680dC75b173|
+| UZHSushi (UZHSUS) | 0x8182965A5dC302e6b25b2b177c7CCa42C5099795 |
+| UZHCro (UZHCro) | 0x90aF2F7f19A93fc80D4F983218C56Bc2f8544989 |
+| Deployed Incoingnito (ICG)| 0xEe9E427945A073c9C8801dC5da44a276aF339333|
+|Deployed Intellicoin (ITC)| 0x2A35E060849Fa56Ba648C93a50E23359b5d14515 |
+| Deployed Privatepedia (PVT) | 0x5e1bcb66D6CbFA4F98bB63BaF4357a543232BFbc | 
+| Deployed Coinicious (CNS) |  0xC486C817bE36F9ccf257BfF86CC33ff71a69D651 |
+| Deployed Cryptofficialcoin (COC) | 0xd0b00725255C35514A8d702b4B4F78C141E8B5eF |
--- scripts/deploy-erc20-tokens.ts
+++ scripts/deploy-erc20-tokens.ts
@@ -4,57 +4,50 @@ import { ethers } from "hardhat";
 import * as fs from "fs";
 
 async function main() {
-  let addresses = {};
+  const addresses: any = {};
 
   // deploy UZHETH tokens
   const UzhUniToken = await ethers.getContractFactory("UzhUniToken");
   const uzhuni = await UzhUniToken.deploy();
   await uzhuni.deployed();
   console.log("Deployed UzhUniToken (UZHUNI): ", uzhuni.address);
-  // eslint-disable-next-line node/no-unsupported-features/es-syntax
-  addresses = { ...addresses, uzhuni: { address: uzhuni.address } };
+  addresses.UzhUniToken = { address: uzhuni.address };
 
   const UZHSushiToken = await ethers.getContractFactory("UZHSushi");
   const uzhushi = await UZHSushiToken.deploy();
   await uzhushi.deployed();
   console.log("Deployed UZHSushi:", uzhushi.address);
-  // eslint-disable-next-line node/no-unsupported-features/es-syntax
-  addresses = { ...addresses, uzhuni: { address: uzhuni.address } };
+  addresses.UZHSushi = { address: uzhushi.address };
 
   const UZHCroToken = await ethers.getContractFactory("UZHCro");
   const uzhcro = await UZHCroToken.deploy();
   await uzhcro.deployed();
   console.log("Deployed UZHCro:", uzhcro.address);
-  // eslint-disable-next-line node/no-unsupported-features/es-syntax
-  addresses = { ...addresses, uzhcro: { address: uzhcro.address } };
+  addresses.UZHCro = { address: uzhcro.address };
 
   const Incoingnito = await ethers.getContractFactory("Incoingnito");
   const incoingnito = await Incoingnito.deploy();
   await incoingnito.deployed();
   console.log("Deployed Incoingnito (ICG): ", incoingnito.address);
-  // eslint-disable-next-line node/no-unsupported-features/es-syntax
-  addresses = { ...addresses, incoingnito: { address: incoingnito.address } };
+  addresses.Incoingnito = { address: incoingnito.address };
 
   const Intellicoin = await ethers.getContractFactory("Intellicoin");
   const intellicoin = await Intellicoin.deploy();
   await intellicoin.deployed();
   console.log("Deployed Intellicoin (ITC): ", intellicoin.address);
-  // eslint-disable-next-line node/no-unsupported-features/es-syntax
-  addresses = { ...addresses, intellicoin: { address: intellicoin.address } };
+  addresses.Intellicoin = { address: intellicoin.address };
 
   const Privatepedia = await ethers.getContractFactory("Privatepedia");
   const privatepedia = await Privatepedia.deploy();
   await privatepedia.deployed();
   console.log("Deployed Privatepedia (PVT): ", privatepedia.address);
-  // eslint-disable-next-line node/no-unsupported-features/es-syntax
-  addresses = { ...addresses, privatepedia: { address: privatepedia.address } };
+  addresses.Privatepedia = { address: privatepedia.address };
 
   const Coinicious = await ethers.getContractFactory("Coinicious");
   const coinicious = await Coinicious.deploy();
   await coinicious.deployed();
   console.log("Deployed Coinicious (CNS): ", coinicious.address);
-  // eslint-disable-next-line node/no-unsupported-features/es-syntax
-  addresses = { ...addresses, coinicious: { address: coinicious.address } };
+  addresses.Coinicious = { address: coinicious.address };
 
   const Cryptofficialcoin = await ethers.getContractFactory(
     "Cryptofficialcoin"
@@ -62,27 +55,22 @@ async function main() {
   const cryptofficialcoin = await Cryptofficialcoin.deploy();
   await cryptofficialcoin.deployed();
   console.log("Deployed Cryptofficialcoin (COC): ", cryptofficialcoin.address);
-  // eslint-disable-next-line node/no-unsupported-features/es-syntax
-  addresses = {
-    // eslint-disable-next-line node/no-unsupported-features/es-syntax
-    ...addresses,
-    cryptofficialcoin: { address: cryptofficialcoin.address },
-  };
+  addresses.Cryptofficialcoin = { address: cryptofficialcoin.address };
 
   // write file to json
   const input = { date: new Date(), tokens: addresses };
-  const file: string = "./tokenAddress.json";
+  const file: string = "./logs/tokenAddress.json";
   fs.stat(file, (exist) => {
     if (exist) {
       try {
-        fs.appendFileSync("./tokenAddresses.json", JSON.stringify(input));
+        fs.appendFileSync("./logs/tokenAddresses.json", JSON.stringify(input));
         console.log("appended - addresses successfully written in file!");
       } catch (err) {
         console.error(err);
       }
     } else {
       try {
-        fs.writeFileSync("./tokenAddresses.json", JSON.stringify(input));
+        fs.writeFileSync("./logs/tokenAddresses.json", JSON.stringify(input));
         console.log("created - addresses successfully written in file!");
       } catch (err) {
         console.error(err);
