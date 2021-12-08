5501a0ed352ddfe9216accdca28fb11374cdb5c8
Jonas Brunner <jonas.brunner@outlook.com>
2021-12-08 21:59:05 +0100

    ensure directory exists before writing file

--- helpers/write-files.ts
+++ helpers/write-files.ts
@@ -1,4 +1,14 @@
 import fs from "fs";
+import path from "path";
+
+function ensureDirectoryExistence(filePath: string) {
+  var dirname = path.dirname(filePath);
+  if (fs.existsSync(dirname)) {
+    return true;
+  }
+  ensureDirectoryExistence(dirname);
+  fs.mkdirSync(dirname);
+}
 
 // write values to json file logs
 const writeLogFile = (
@@ -12,6 +22,7 @@ const writeLogFile = (
   console.log("writing log file...");
   if (fs.existsSync(fullPath)) {
     try {
+      ensureDirectoryExistence(fullPath);
       fs.appendFileSync(fullPath, JSON.stringify(input));
       console.log(`appended - successfully written in ${fullPath}!`);
     } catch (err) {
@@ -19,6 +30,7 @@ const writeLogFile = (
     }
   } else {
     try {
+      ensureDirectoryExistence(fullPath);
       fs.writeFileSync(fullPath, JSON.stringify(input));
       console.log(`created - successfully written in ${fullPath}!`);
     } catch (err) {
